00e16bb8f690135de0adb85f2d6f487c15ab962d
Engineer-kim <ddang0103@naver.com>
2025-01-08 19:20:10 +0900

    Fix: 브랜치 꼬여서 삭제되부분 원복 커밋

---
+++ example01/example01/multiThreading.java
@@ -0,0 +1,36 @@
+package example01;
+
+public class multiThreading {
+
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(new Runnable(){ //Thread 객체 생성 시 Runnable 객체를 인자로 전달하여 스레드와 작업을 연결
+            @Override
+            public void run(){
+                //Code that will run in a new thread
+                System.out.println("처음");
+                System.out.println("Current " + Thread.currentThread().getPriority());
+                throw new RuntimeException("일부러 오류냄");
+            }
+        });
+
+        thread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler(){
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                System.out.println("A critical error happend in thread"  + t.getName()
+                + " the error is" + e.getMessage());
+            }
+        });
+
+
+        thread.setName("new Worker Thread");
+        thread.setPriority(Thread.MAX_PRIORITY);
+
+        System.out.println("2번째" );
+        thread.start();// thread.start()를 호출해야 실제로 스레드가 실행됨
+        System.out.println("3번째" );
+        Thread.sleep(10000);// 이시간이 지날때까지는 현재 스레드를 스케줄링하지 않는다
+    }
+
+
+}
---
+++ example01/example06/Main.java
@@ -0,0 +1,74 @@
+package example06;
+
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+//join()은 다른 스레드가 작업을 마칠 때까지 기다리는 "대기실"과 같습니다. 여러 사람이 동시에 작업을 하는데,
+// 한 사람이 다른 사람의 작업 결과가 필요하다면 그 사람이 작업을 끝낼 때까지 기다려야 합니다.
+// 이때 join()을 사용하여 다른 사람(스레드)이 작업을 마칠 때까지 기다리는 것입니다.
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        List<Long> inputNumbers = Arrays.asList(100000000L, 3435L, 35435L, 2324L, 4656L, 23L, 5556L);
+
+        List<FactorialThread> threads = new ArrayList<>();
+
+        for (long inputNumber : inputNumbers) {
+            threads.add(new FactorialThread(inputNumber)); //입력 숫자 목록을 반복하면서 각 숫자에 대해 FactorialThread 객체를 생성하고 리스트에 추가
+        }
+
+        for (Thread thread : threads) {
+            thread.setDaemon(true); //데몬 스레드는 프로그램의 주요 스레드가 종료되면 자동으로 종료됩니다.
+            thread.start();
+        }
+
+        for (Thread thread : threads) {
+            //호출된 스레드의 run() 메소드가 끝날 때까지 현재 스레드는 블록
+            thread.join(2000);//각 스레드가 완료될 때까지 기다립니다. 최대 2초 동안 기다리며, 만약 스레드가 아직 작업 중이라면 다음 스레드로 넘어갑니다.
+        }
+
+        for (int i = 0; i < inputNumbers.size(); i++) {
+            FactorialThread factorialThread = threads.get(i);
+            if (factorialThread.isFinished()) {
+                System.out.println("Factorial of " + inputNumbers.get(i) + " is " + factorialThread.getResult());
+            } else {
+                System.out.println("The calculation for " + inputNumbers.get(i) + " is still in progress");
+            }
+        }
+    }
+
+    public static class FactorialThread extends Thread {
+        private long inputNumber;
+        private BigInteger result = BigInteger.ZERO;
+        private boolean isFinished = false;
+
+        public FactorialThread(long inputNumber) {
+            this.inputNumber = inputNumber;
+        }
+
+        @Override
+        public void run() {
+            this.result = factorial(inputNumber);
+            this.isFinished = true;
+        }
+
+        public BigInteger factorial(long n) {
+            BigInteger tempResult = BigInteger.ONE;
+
+            for (long i = n; i > 0; i--) {
+                tempResult = tempResult.multiply(new BigInteger((Long.toString(i))));
+            }
+            return tempResult;
+        }
+
+        public BigInteger getResult() {
+            return result;
+        }
+
+        public boolean isFinished() {
+            return isFinished;
+        }
+    }
+}
